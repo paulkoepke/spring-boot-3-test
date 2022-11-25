c13325f11365552f1529368b2a63e29868ce0290
paulkoepke <paul.koepke@t-online.de>
2022-11-25 13:24:48 +0100

    init

---
+++ src/main/java/com/example/sprinb3/Sprinb3Application.java
@@ -0,0 +1,13 @@
+package com.example.sprinb3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprinb3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprinb3Application.class, args);
+	}
+
+}
